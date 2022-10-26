04f3bcedf35c059132c20ba98084e94edf25a9a6
Asti <astistateofmind@gmail.com>
2022-10-26 13:29:38 -0700

    add stlye

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,7 +4,7 @@ export default function MovieList(props) {
   return (
     <>
       {props.movies.map((movie, index) => (
-        <div className="image-container d-flex justify-content-start m3">
+        <div className="image-container d-flex flex-wrap justify-content-around align-items-center m-3">
           <img src={movie.Poster} alt="movie" />
         </div>
       ))}
