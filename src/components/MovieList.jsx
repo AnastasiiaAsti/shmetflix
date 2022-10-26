05ad34883623c23d4dd0554c0622cae28033f164
Asti <astistateofmind@gmail.com>
2022-10-26 13:59:03 -0700

    change in layout in movielist

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,7 +5,7 @@ export default function MovieList(props) {
   return (
     <>
       {props.movies.map((movie, index) => (
-        <div className="image-container d-flex flex-wrap justify-content-around align-items-center m-3">
+        <div className="image-container col-auto d-flex flex-wrap justify-content-center align-items-center m-3">
           <img src={movie.Poster} alt="movie" />
           <div
             onClick={() => props.handleFavouriteClick(movie)}
